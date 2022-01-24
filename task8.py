cfb49b0eb3cfbaa7d5d8435d7db51b1144e193f9
Asya <nastya.Z1998@yandex.ru>
2022-01-24 09:24:07 +0500

    8

--- task8.py
+++ task8.py
@@ -3,6 +3,7 @@
 def show_even_numbers (n):
     list = []
     for i in range (1, n + 1):
-        list.append(i)
+        if i % 2 == 0:
+            list.append(i)
     return (list)
 print(show_even_numbers(10))
